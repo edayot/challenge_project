65f9fdeb6f63f16bfcfe5e9f984431a021f4eb3d
edayot <pro.e.dayot@gmail.com>
2024-05-31 16:23:31 +0200

    ptdr ca marche pas

--- scripts/control.py
+++ scripts/control.py
@@ -3,40 +3,70 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Float32MultiArray, Float32
 import numpy as np
+from std_msgs.msg import Float32MultiArray
+
+from math import sqrt
+
+OBJECTIVE_ANGLE = None
+DIFF_G_D = None
+SIGNED_MIN_G_D = None
+DISTANCE_FRONT = None
+FIRST_MSG_RECEIVED = False
 
 
-LAST_ANGLE = 0
+
+def callback(msg : Float32MultiArray):
+    global OBJECTIVE_ANGLE, DIFF_G_D, SIGNED_MIN_G_D, DISTANCE_FRONT
+    OBJECTIVE_ANGLE, DIFF_G_D, SIGNED_MIN_G_D, DISTANCE_FRONT = msg.data
+    global FIRST_MSG_RECEIVED
+    FIRST_MSG_RECEIVED = True
 
 
-
-def callback(msg : Float32):
-    global LAST_ANGLE
-    LAST_ANGLE = msg.data
-
-
-
+def signe(x):
+    if x > 0:
+        return 1
+    if x < 0:
+        return -1
+    return 0
 
 
 def main():
     pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     rospy.init_node("control", anonymous=True)
 
-    rospy.Subscriber("/distance", Float32, callback)
+    rospy.Subscriber("/distance", Float32MultiArray, callback)
 
-    v_linear = 0.2
-    v_angular_factor = 1/25
+    v_linear = 0.1
+    v_angular_factor = 1/20
 
+    DISTANCE_FRONT_DIM = 0.2
+    DISTANCE_FRONT_MIN = 0.05
+    rate = rospy.Rate(2)
     while not rospy.is_shutdown():
+        rate.sleep()
         msg = Twist()
         msg.linear.x = v_linear
-        
-        v_angular = LAST_ANGLE * v_angular_factor
 
-        msg.angular.z = v_angular
+        if FIRST_MSG_RECEIVED:
+            if DISTANCE_FRONT < DISTANCE_FRONT_DIM:
+                msg.linear.x = v_linear * DISTANCE_FRONT / DISTANCE_FRONT_DIM
+            if DISTANCE_FRONT < DISTANCE_FRONT_MIN:
+                msg.linear.x = 0
+            
+            mur_coef = DIFF_G_D * 3
+            obj_coef = 2*sqrt(abs(OBJECTIVE_ANGLE)) * signe(OBJECTIVE_ANGLE)
+            coef = 0
+            if abs(mur_coef) > abs(obj_coef):
+                coef = mur_coef
+            else:
+                coef = obj_coef + mur_coef
+            if abs(SIGNED_MIN_G_D) < 0.15:
+                coef = 1/-SIGNED_MIN_G_D /2
+            msg.angular.z = v_angular_factor * (coef)
+            pub.publish(msg)
 
-        pub.publish(msg)
+            
 
 
 
--- scripts/lds_distance.py
+++ scripts/lds_distance.py
@@ -6,66 +6,64 @@ import rospy
 from sensor_msgs.msg import LaserScan
 import numpy as np
 
-from std_msgs.msg import Float32MultiArray, Float32
+from std_msgs.msg import Float32MultiArray
 
 def callback(msg : LaserScan):
-    beam_angle = 30
+    objective_angle = calc_objective_angle(msg)
+    diff_g_d, signed_min_g_d = calc_diff_g_d(msg)
+    distance_front = calc_distance_front(msg)
+    msg = Float32MultiArray()
+    msg.data = [objective_angle, diff_g_d, signed_min_g_d, distance_front]
+    pub.publish(msg)
 
-    front_list : list = msg.ranges[360-(beam_angle+1):] + msg.ranges[:beam_angle]
-
+def calc_diff_g_d(msg : LaserScan):
     beam_angle = 10
-    back_list : list = msg.ranges[180-beam_angle:180+beam_angle]
     left_list : list = msg.ranges[90-beam_angle:90+beam_angle]
     right_list : list = msg.ranges[270-beam_angle:270+beam_angle]
 
     min_left = min(left_list)
     min_right = min(right_list)
 
-    n_infinity =  0
+    diff = min_left - min_right
+    signed_min_g_d = diff / abs(diff) * min(abs(min_left), abs(min_right))
+    return diff, - signed_min_g_d
+
+def calc_distance_front(msg : LaserScan):
+    beam_angle = 10
+    front_list : list = msg.ranges[360-(beam_angle+1):] + msg.ranges[:beam_angle]
+
+    return min(front_list)
+
+
+
+def calc_objective_angle(msg : LaserScan):
+    beam_angle = 30
+    front_list : list = msg.ranges[360-(beam_angle+1):] + msg.ranges[:beam_angle]
+
+    n_inf = 0
     for i in range(len(front_list)):
         if front_list[i] == np.inf:
-            n_infinity += 1
+            n_inf += 1
+    if n_inf > beam_angle:
+        return 0    
     front_list = [x if x < 0.8 else 0 for x in front_list]
     front_list = list(front_list)
-
-    # set 0 to the middle of the list
     n = len(front_list)
-    offset = 1
+    offset = 10
     for i in range(n):
         if n/2 - offset < i < n/2 + offset:
             front_list[i] = 0
-
     max_front_index = front_list.index(max(front_list))
-
     angle = 0 - len(front_list) // 2 + max_front_index
-    angle *= 2
+    return angle
 
-    if max(front_list) == 0:
-        angle = 0
-
-    diff = min_left - min_right
-    if diff != np.inf:
-        angle = angle + diff * 200
-
-    if n_infinity > 30:
-        angle = angle / (n_infinity)
-    if n_infinity > 50:
-        angle = 0
-    if np.isnan(angle):
-        angle = 0
-
-    
-    msg = Float32(angle)
-    pub.publish(msg)
-
-    
 
 
 def main():
     rospy.init_node("lds_distance", anonymous=True)
 
     global pub
-    pub = rospy.Publisher("/distance", Float32, queue_size=10)
+    pub = rospy.Publisher("/distance", Float32MultiArray, queue_size=10)
     rospy.Subscriber("/scan", LaserScan, callback)
     rospy.spin()
 
